004f135b94ab906e49da806c83db4e1d4f58d034
tymon <prem89@gmail.com>
2015-11-17 20:46:31 +0530

    Commit from eclipse

--- src/com/prem/misc/HelloWorld.java
+++ src/com/prem/misc/HelloWorld.java
@@ -3,7 +3,7 @@ package com.prem.misc;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hi..GitHub!!");
+		System.out.println("Hi..GitHub!! Added thru Eclipse");
 	}
 
 }
